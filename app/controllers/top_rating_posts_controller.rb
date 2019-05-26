557be43aba7fe270d1979241c8b02ee2a5c43e76
Grechko Dima <dima@Grechkos-MacBook-Pro.local>
2019-05-27 01:37:42 +0300

    [Refactoring] refactor quotes and frozen_string_literal

--- app/controllers/top_rating_posts_controller.rb
+++ app/controllers/top_rating_posts_controller.rb
@@ -4,9 +4,8 @@ class TopRatingPostsController < ApplicationController
   def index
     number_of_posts = params.fetch(:number, 20)
     top_rating_posts = TopRatingPost.order('average_rating DESC').limit(number_of_posts)
-    render json: { status: 200, post_data: PostSerializer.new(top_rating_posts,
-                                                              params: { skip_author_id: true }).serializable_hash },
-           status: :ok
+    top_posts_json = PostSerializer.new(top_rating_posts, params: { skip_author_id: true }).serializable_hash
+    render json: { status: 200, post_data: top_posts_json }, status: :ok
   end
 
   private
